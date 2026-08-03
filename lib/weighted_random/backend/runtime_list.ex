6609001ed210ce85329d2136ca4388ae46dd6d44
greetingsfellowhumans <accounts@aaronjprice.com>
2026-08-02 19:29:53 -0700

    refactor: cleanup backend modules

--- lib/weighted_random/backend/runtime_list.ex
+++ lib/weighted_random/backend/runtime_list.ex
@@ -3,9 +3,6 @@ defmodule WeightedRandom.Backend.RuntimeList do
 
   defstruct [
     li: [],
-    # int_map: [],
-    # weights: [],
-    # opts: [],
   ]
 
   @impl true
@@ -26,20 +23,5 @@ defmodule WeightedRandom.Backend.RuntimeList do
   def take(%__MODULE__{li: li}, count) do
     Enum.take_random(li, count)
   end
-
-
-  #defp rand_ints(li, weights, opts) do
-  #  custom_weights = Enum.map(weights, &Weight.new(&1, opts))
-  #  side_effects = Enum.map(custom_weights, &Weight.create_side_effect_weights/1)
-
-  #  li =
-  #    (Weight.convert_to_values(custom_weights) ++
-  #       Weight.convert_to_values(side_effects) ++
-  #       Enum.map(li, & &1))
-  #    |> Enum.filter(&(&1 in li))
-
-  #  Enum.random(li)
-  #end
-
 
 end
