e239f628bd394406dfb01ce763f4187fe04c19da
greetingsfellowhumans <accounts@aaronjprice.com>
2026-08-20 17:25:53 -0700

    feat(deps): add dependency :nimble_options ~> 1.0

--- mix.exs
+++ mix.exs
@@ -63,6 +63,7 @@ defmodule WeightedRandom.MixProject do
     [
       {:wam, "~> 0.1.0"},
       {:curves, "~> 0.2.3"},
+      {:nimble_options, "~> 1.0"},
       {:benchee, "~> 1.5", only: [:dev, :test]},
       {:ex_doc, "~> 0.40", only: :dev, runtime: false},
       {:vega_lite, "~> 0.1.0", only: :dev, runtime: false},
