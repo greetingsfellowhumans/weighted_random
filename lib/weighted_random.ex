619f873b803669cb1f67b117aaeb2800702d388c
greetingsfellowhumans <accounts@aaronjprice.com>
2026-08-25 08:46:13 -0700

    Better docs

--- lib/weighted_random.ex
+++ lib/weighted_random.ex
@@ -7,22 +7,75 @@ defmodule WeightedRandom do
 
   The best way to learn is through the interactive [Livebook tutorial](guides/tutorial.livemd)
 
-  ## Usage
+  ## Quickstart
+
+  ### Randomness from outcomes and weights.
 
   ```elixir
-  outcomes = 0..100
-  weight = %{target: 25, weight: 50, radius: 10}
+  outcomes = 0..10
+  weight = %{target: 5, weight: 20}
   r = WeightedRandom.preprocess(0..100, [weight])
-  [n1, n2, n3, n4] = WeightedRandom.take(r, 4)
+
+  count = 8
+  [4, 5, 5, 5, 4, 1, 5, 5] = WeightedRandom.take(r, count)
   ```
 
-  Alternately, if you care less about performance, you can do it all at once:
+  Alternately, if you care less about performance, and only need to do it once:
   ```elixir
-  [n1, n2, n3, n4] = WeightedRandom.rand(0..100, [%{target: 25, weight: 50, radius: 10}], [take: 4])
+  [5, 5, 2] = WeightedRandom.rand(0..10, [%{target: 5, weight: 20}], take: 3)
   ```
 
-  But please note the algorithm is optimized to take a long time ( O(n) ) during preprocessing, in order to be very fast during the sampling step ( O(1) ).
-  So it will be far better to preprocess once, and take many times. `rand/3` preprocesses EVERY time it is called.
+  But please note the algorithm is optimized to take longer ( O(n) ) during preprocessing, in order to be very fast ( O(1) ) during the sampling step.
+  So it will be far better to use `WeightedRandom.preprocess/3` once, and `WeightedRandom.take/2` many times. `rand/3` preprocesses EVERY time it is called.
+
+
+  ### Randomness from probabilities
+  Mirroring the functions above, we can also use the `*_p` functions to use probabilities instead of outcomes + weights
+
+  ```elixir
+
+
+  probabilities = [0.01, 0.01, 0.01, 0.95, 0.01, 0.01]
+  r = WeightedRandom.preprocess_p(probabilities)
+
+  count = 5
+  [3, 3, 3, 3, 3] = WeightedRandom.take(r, count)
+  ```
+
+  Or, in rand form (with the same disclaimer)
+
+  ```elixir
+
+  probabilities = [0.01, 0.01, 0.01, 0.95, 0.01, 0.01]
+  count = 5
+  [3, 3, 3, 3, 3] = WeightedRandom.rand_p(r, count)
+
+  ```
+
+
+
+
+
+  ## Weight vs Probability
+  Weights eventually get converted into probabilities under the hood. Here are a few differences to keep in mind.
+
+  ### Adding to one takes from others
+  You can *add* weights to a target to make it more likely to appear as a result. Doing so makes ALL other outcomes less likely to appear as results, an they are all affected evenly.
+
+  Probabilities are floats that should add up to 1.0 (within a reasonable margin of rounding error. More on that later).
+  So any time you increase one probability, you need to *subtract* from one or many of the other probabilities.
+  This gives you finer control, but is more manual work and risk of human error.
+
+  ### Coupling of outcomes
+
+  Weights force you to manually pass in a list of possible outcomes.
+  *The target of a weight is an index of an outcome*
+  So if your outcomes are `100..200` then a weight of `%{target: 4, ...}` would influence the outcome `104`, not the literal value `4`.
+  You can change that by passing in the option `[outcome_type: :value]`. Now the weight should be `%{target: 104, ...}`
+
+  Probabilities on the other hand implicitly create a list of outcomes from their indices.
+  So if `probabilities = [0.75, 0.25]`, then we have two possible outcomes: `0` with a 75% chance, and `1` with a 25% chance.
+
   """
 
 
